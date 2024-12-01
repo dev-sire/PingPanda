c744fdd0556c027bf4f5a32000b3692887f338d2
Aman Shahid <shahidamaan2019@gmail.com>
2024-12-01 23:23:28 +0500

    completed landing page

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,13 +1,32 @@
 import { Heading } from "@/components/heading";
 import { MaxWidthWrapper } from "@/components/max-width-wrapper";
-import { Check } from "lucide-react";
+import { Check, Star, StarHalf } from "lucide-react";
 import { ShinyButton } from "@/components/shiny-button";
 import { MockDiscordUI } from "@/components/mock-discord-ui";
 import { AnimatedList, AnimatedListItem } from "@/components/ui/animated-list";
 import { DiscordMessage } from "@/components/discord-message";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 import Image from "next/image";
+import { Icons } from "@/components/icons";
 
 const Page = () => {
+
+	const codeSnippet = `await fetch("http://localhost:3000/api/v1/events", {
+		method: "POST",
+		body: JSON.stringify({
+		  category: "sale",
+		  fields: {
+			plan: "PRO",
+			email: "zoe.martinez2001@email.com",
+			amount: 49.00
+		  }
+		}),
+		headers: {
+		  Authorization: "Bearer <YOUR_API_KEY>"
+		}
+	})`
+
 	return (
 		<>
 			<section className="relative py-24 sm:py-32 bg-brand-25">
@@ -107,7 +126,7 @@ const Page = () => {
 						<h2 className="text-center text-base/7 font-semibold text-brand-600">
 							Intuitive Monitoring
 						</h2>
-						<Heading>Stay ahead of real-time insights</Heading>
+						<Heading className="text-center">Stay ahead of real-time insights</Heading>
 					</div>
 					<div className="grid gap-4 lg:grid-cols-3 lg:grid-rows-2">
 						{/* First Bento grid element */}
@@ -116,7 +135,7 @@ const Page = () => {
 							<div className="relative flex flex-col h-full overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] lg:rounded-l-[calc(2rem+1px)]">
 								<div className="pb-3 pt-8 px-8 sm:px-10 sm:pb-0 sm:pt-10">
 									<p className="mt-2 font-medium text-lg/7 tracking-tight text-brand-950 max-lg:text-center">
-										Real-time notification
+										Real-time Notification
 									</p>
 									<p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
 										Get notified about critical events the moment they happen, no matter if you&apos;re home or at the go
@@ -130,10 +149,152 @@ const Page = () => {
 							</div>
 							<div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 lg:rounded-l-[2rem]" />
 						</div>
+						{/* Second Bento grid element */}
+						<div className="relative max-lg:row-start-1">
+							<div className="absolute inset-px rounded-lg bg-white max-lg:rounded-t-[2rem]" />
+							<div className="relative flex flex-col h-full overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]">
+								<div className="px-8 pt-8 sm:px-10 sm:pt-10">
+									<p className="mt-2 font-medium text-lg/7 tracking-tight text-brand-950 max-lg:text-center">
+										Track Any Event
+									</p>
+									<p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+										From new user signups to successful payments, PingPanda notifies you for all critical events in your SaaS
+									</p>
+								</div>
+								<div className="flex flex-1 items-center justify-center px-8 max-lg:pb-12 max-lg:pt-10 sm:px-10 lg:pb-2">
+									<Image className="w-full max-lg:max-w-xs" src="/bento-any-event.png" alt="event tracking" width={500} height={300} />
+								</div>
+							</div>
+							<div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 max-lg:rounded-t-[2rem]" />
+						</div>
+						{/* Third Bento Grid Element */}
+						<div className="relative row-start-3 lg:col-start-2 lg:row-start-2">
+							<div className="absolute inset-px rounded-lg bg-white" />
+							<div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)]">
+								<div className="px-8 pt-8 sm:px-10 sm:pt-10">
+									<p className="mt-2 font-medium text-lg/7 tracking-tight text-brand-950 max-lg:text-center">
+										Track Any Properties
+									</p>
+									<p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+										Any custom data you like to an event, such as a user email, a purchase amount, or an exceeded quota.
+									</p>
+								</div>
+								<div className="flex flex-1 items-center justify-center px-8 max-lg:pb-12 max-lg:pt-10 sm:px-10 lg:pb-2">
+									<Image className="w-full max-lg:max-w-xs" src="/bento-custom-data.png" alt="Custom Data" width={500} height={300} />
+								</div>
+							</div>
+							<div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5" />
+						</div>
+						{/* Fourth Bento Grid Element */}
+						<div className="relative lg:row-span-2">
+							<div className="absolute inset-px rounded-lg bg-white max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]" />
+							<div className="relative flex flex-col h-full overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)]">
+								<div className="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
+									<p className="mt-2 font-medium text-lg/7 tracking-tight text-brand-950 max-lg:text-center">
+										Easy Integration
+									</p>
+									<p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+										Connect PingPanda with your existing workflows in minutes and call our intuitive logging API from any language.
+									</p>
+								</div>
+								<div className="relative min-h-[30rem] w-full grow">
+									<div className="absolute bottom-0 left-10 right-0 top-10 overflow-hidden rounded-tl-xl bg-gray-900 shadow-2xl">
+										<div className="flex bg-gray-800/40 ring-1 ring-white/5">
+											<div className="-mb-px flex font-medium text-sm/6 text-gray-400">
+												<div className="border-b border-r border-b-white/20 border-r-white/10 bg-white/5 px-4 py-2 text-white">
+													pingpanda.js
+												</div>
+											</div>
+										</div>
+										<div className="overflow-hidden">
+											<div className="max-h-[30rem]">
+												<SyntaxHighlighter 
+													language="typescript"
+													style={{
+														...oneDark,
+														'pre[class*="language-"]': {
+															...oneDark['pre[class*="language-"]'],
+															background: "transparent",
+															overflow: "hidden",
+														},
+														'code[class*="language-"]': {
+															...oneDark['code[class*="language-"]'],
+															background: "transparent",
+														},
+													}}
+												>
+													{codeSnippet}
+												</SyntaxHighlighter>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 max-lg:rounded-b-[2rem]" />
+						</div>
 					</div>
 				</MaxWidthWrapper>
 			</section>
-			<section></section>
+			<section className="relative py-24 sm:py-32 bg-white">
+				<MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-20">
+					<div>
+						<h2 className="text-center text-base/7 font-semibold text-brand-600">
+							Real-world Experiences
+						</h2>
+						<Heading className="text-center">What our customers say</Heading>
+					</div>
+					<div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 divide-y lg:divide-y-0 lg:divide-x divide-gray-200">
+						{/* First customer review */}
+						<div className="flex flex-auto flex-col gap-4 bg-brand-25 p-6 sm:p-8 lg:p-16 rounded-t-[2rem] lg:rounded-tr-none lg:rounded-l-[2rem]">
+							<div className="flex gap-0.5 mb-2 justify-center lg:justify-start">
+								<Star className="size-5 text-brand-600 fill-brand-600" />
+								<Star className="size-5 text-brand-600 fill-brand-600" />
+								<Star className="size-5 text-brand-600 fill-brand-600" />
+								<Star className="size-5 text-brand-600 fill-brand-600" />
+								<Star className="size-5 text-brand-600 fill-brand-600" />
+							</div>
+							<p className="text-base sm:text-lg lg:text-lg/8 font-medium tracking-tight text-brand-950 text-center lg:text-left text-pretty">
+								PingPanda has been a game-changer for me. I've been using it for two months and seeing sales pop-up in real-time is super satisfying.
+							</p>
+							<div className="flex flex-col justify-center lg:justify-start sm:flex-row items-center sm:items-start gap-4 mt-2">
+								<Image src="/user-2.png" alt="user 2 image" className="rounded-full object-cover" width={48} height={48} />
+								<div className="flex flex-col items-center sm:items-start">
+									<p className="font-semibold flex items-center">
+										Freya Larsson
+										<Icons.verificationBadge className="size-4 inline-block ml-1.5" />
+									</p>
+									<p className="text-sm text-gray-600">@itsfreya</p>
+								</div>
+							</div>
+						</div>
+						{ /* Second customer review */ }
+						<div className="flex flex-auto flex-col gap-4 bg-brand-25 p-6 sm:p-8 lg:p-16 rounded-b-[2rem] lg:rounded-bl-none lg:rounded-r-[2rem]">
+							<div className="flex gap-0.5 mb-2 justify-center lg:justify-start">
+								<Star className="size-5 text-brand-600 fill-brand-600" />
+								<Star className="size-5 text-brand-600 fill-brand-600" />
+								<Star className="size-5 text-brand-600 fill-brand-600" />
+								<Star className="size-5 text-brand-600 fill-brand-600" />
+							</div>
+							<p className="text-base sm:text-lg lg:text-lg/8 font-medium tracking-tight text-brand-950 text-center lg:text-left text-pretty">
+								PingPanda is paying off for our SaaS. Nice to have a simple to see what we're doing day-to-day. Definitely makes our lives easier.
+							</p>
+							<div className="flex flex-col justify-center lg:justify-start sm:flex-row items-center sm:items-start gap-4 mt-2">
+								<Image src="/user-1.png" alt="user 2 image" className="rounded-full object-cover" width={48} height={48} />
+								<div className="flex flex-col items-center sm:items-start">
+									<p className="font-semibold flex items-center">
+										Kai Durant
+										<Icons.verificationBadge className="size-4 inline-block ml-1.5" />
+									</p>
+									<p className="text-sm text-gray-600">@ig_kai</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<ShinyButton href="/sign-up" className="relative h-14 z-10 w-full max-w-xs text-base shadow-lg transition-shadow duration-300 hover:shadow-xl">
+						Start For Free Today
+					</ShinyButton>
+				</MaxWidthWrapper>
+			</section>
 		</>
 	)
 }
