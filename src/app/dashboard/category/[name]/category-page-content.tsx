633fcea64a5aab049aba965018f524acac1bca8c
Aman Shahid <shahidamaan2019@gmail.com>
2024-12-22 22:02:25 +0500

    started-integrating-advanced-table

--- src/app/dashboard/category/[name]/category-page-content.tsx
+++ src/app/dashboard/category/[name]/category-page-content.tsx
@@ -3,12 +3,15 @@
 import { EventCategory } from "@prisma/client";
 import { useQuery } from "@tanstack/react-query";
 import { EmptyCategoryState } from "./empty-category-state";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import { client } from "@/lib/client";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Card } from "@/components/ui/card";
-import { BarChart } from "lucide-react";
+import { ArrowUpDown, BarChart } from "lucide-react";
+import { isAfter, isToday, startOfMonth, startOfWeek } from "date-fns";
+import { ColumnDef } from "@tanstack/react-table";
+import { Button } from "@/components/ui/button";
 
 interface CategoryPageContentProps {
     hasEvents: boolean;
@@ -39,6 +42,25 @@ export const CategoryPageContent = ({ hasEvents: initialHasEvents, category }: C
         )
     }
 
+    const columns: ColumnDef<Event>[] = useMemo(() => [
+        {
+            accessorKey: "category",
+            header: "category",
+            cell: () => <span>{category.name || "uncategorized"}</span>
+        },
+        {
+            accessorKey: "createdAt",
+            header: ({ column }) => {
+                return(
+                    <Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+                        Date
+                        <ArrowUpDown className="ml-2 size-4" />
+                    </Button>
+                )
+            }
+        }
+    ], []) 
+
     const {data, isFetching} = useQuery({
         queryKey: ["events", category.name, pagination.pageIndex, pagination.pageSize, activeTab],
         queryFn: async () => {
@@ -53,6 +75,73 @@ export const CategoryPageContent = ({ hasEvents: initialHasEvents, category }: C
         refetchOnWindowFocus: false,
         enabled: pollingData.hasEvents,
     })
+
+    const numericFieldsSums = useMemo(() => {
+        if(!data?.events || data.events.length === 0) return {}
+
+        const sums: Record<string, {
+            total: number,
+            thisWeek: number,
+            thisMonth: number,
+            today: number
+        }> = {}
+
+        const now = new Date()
+        const weekStart = startOfWeek(now, { weekStartsOn: 0 })
+        const monthStart = startOfMonth(now)
+
+        data.events.forEach((event) => {
+            const eventDate = event.createdAt
+
+            Object.entries(event.fields as object).forEach(([field, value]) => {
+                if(typeof value === "number"){
+                    if(!sums[field]){
+                        sums[field] = { total: 0, thisWeek: 0, thisMonth: 0, today: 0}
+                    }
+
+                    sums[field].total += value
+
+                    if(isAfter(eventDate, weekStart) || eventDate.getTime() === weekStart.getTime()){
+                        sums[field].thisWeek += value
+                    }
+
+                    if(isAfter(eventDate, monthStart) || eventDate.getTime() === monthStart.getTime()){
+                        sums[field].thisMonth += value
+                    }
+
+                    if(isToday(eventDate)){
+                        sums[field].today += value
+                    }
+                }
+            })
+        })
+
+        return sums
+
+    }, [data?.events])
+
+    const NumericFieldSumCards = () => {
+        if(Object.keys(numericFieldsSums).length === 0) return null
+
+        return Object.entries(numericFieldsSums).map(([field, sums]) => {
+            const relevantSum = activeTab === "today" ? sums.today : activeTab === "week" ? sums.thisWeek : sums.thisMonth
+
+            return(
+                <Card key={field}>
+                    <div className="flex flex-row items-center justify-between space-y-0 pb-2">
+                        <p className="text-sm/6 font-medium">{field.charAt(0).toUpperCase() + field.slice(1)}</p>
+                        <BarChart className="size-4 text-muted-foreground" />
+                    </div>
+                    <div>
+                        <p className="text-2xl font-bold">{relevantSum.toFixed(2)}</p>
+                        <p className="text-xs/5 text-muted-foreground">
+                            Events{" "}{activeTab === "today" ? "today" : activeTab === "week" ? "this week" : "this month"}
+                        </p>
+                    </div>
+                </Card>
+            )
+        })
+    }
 
     return(
         <div className="space-y-6">
@@ -74,7 +163,14 @@ export const CategoryPageContent = ({ hasEvents: initialHasEvents, category }: C
                                 <p className="text-sm/6 font-medium">Total Events</p>
                                 <BarChart className="size-4 text-muted-foreground" />
                             </div>
+                            <div>
+                                <p className="text-2xl font-bold">{data?.eventsCount || 0}</p>
+                                <p className="text-xs/5 text-muted-foreground">
+                                    Events{" "}{activeTab === "today" ? "today" : activeTab === "week" ? "this week" : "this month"}
+                                </p>
+                            </div>
                         </Card>
+                        <NumericFieldSumCards />
                     </div>
                 </TabsContent>
             </Tabs>
